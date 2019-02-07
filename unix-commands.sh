23587d3f845dca5b7d7d513c7f8248cfda489aa2
reid-sutherland <reid.sutherland96@gmail.com>
2019-02-07 10:20:14 -0600

    Create unix-commands.sh

---
+++ unix-commands.sh
@@ -0,0 +1,9 @@
+# COPY
+cp /source /dest
+# Copy an entire directory from /source folder to /dest folder
+cp -a /source/. /dest/
+
+# MOVE
+mv /source /dest
+# Move multiple directories to a new directory
+mv dir1 dir2 /path/to/dest/
